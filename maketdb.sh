b3cbc414dd52969db0865a4cd8730f1f8209941b
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-09 21:28:07 +0100

    Some bugfixes and cleanup in the ontology

--- maketdb.sh
+++ maketdb.sh
@@ -1,5 +1,6 @@
 JDIR=$HOME/prg/apache-jena-3.16.0/bin/
 rm -rf tdb
 mkdir -p tdb
+# $JDIR/tdbloader2 --loc tdb Ontology/arm.ttl  Ontology/resources.ttl 
 $JDIR/tdbloader2 --loc tdb Ontology/cieran.ttl 
 
