c806baa3e10896315bdbcc01c73b2735a39abbcf
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-09 22:40:06 +0100

    Added copying of static ontology files

--- payara.sh
+++ payara.sh
@@ -1,6 +1,7 @@
 mvn install || exit
 docker stop armchar
 docker rm armchar
+cp Ontology/*.ttl serverdata/
 cp target/armchar-1.0-SNAPSHOT.war payara/armchar.war
 docker run --name armchar -p 8080:8080 -v `pwd`/payara:/opt/payara/deployments -v `pwd`/serverdata:/opt/payarvara/serverdata  -v `pwd`/tdb:/opt/payarvara/tdb  armchar
 
