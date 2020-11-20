58e849cbaccea35cc88f911c2a75e1ec26df215c
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-20 19:58:56 +0100

    Added missing age logic

---
+++ Ontology/age.r
@@ -0,0 +1,10 @@
+
+
+[ age:
+  ( ?char rdf:type arm:CharacterSheet )
+  ( ?char arm:inYear ?current )
+  ( ?char arm:hasBirthYear ?birth )
+  difference( ?current, ?birth, ?age )
+  ->
+  ( ?char arm:hasAge ?age )
+ ]
