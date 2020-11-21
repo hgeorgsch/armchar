96e7b6216b26708bb50285116fcf024dcd1d6493
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-21 17:53:12 +0100

    Cleaned up the character service.

--- src/main/java/net/schaathun/armchar/resources/Character.java
+++ src/main/java/net/schaathun/armchar/resources/Character.java
@@ -55,69 +55,8 @@ public class Character {
                 .build();
     }
 
-    @POST
-    @Path("")
-    @Consumes("application/x-www-form-urlencoded")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response postCharacter( @FormParam("char") String url) {
-        String queryString = Config.prefix
-                + "CONSTRUCT { \r\n" 
-		+ " <" + url + "> ?p1 ?o1 .  \r\n"
-		+ " ?o1 ?p2 ?o2 . \r\n"
-                + "} WHERE { \r\n " 
-		+ " <" + url + "> ?p1 ?o1 . \r\n"
-		+ " NOT EXISTS { ?p1 rdf:type arm:ignoredProperty }  \r\n"
-                + " OPTIONAL { \r\n" 
-                + "     ?o1 ?p2 ?o2 . \r\n" 
-		+ "     NOT EXISTS { ?o1 rdf:type arm:CharacterSheet }  \r\n"
-		+ "     NOT EXISTS { ?p2 rdf:type arm:ignoredProperty }  \r\n"
-                + " } \r\n"
-                + "}";
-        String result = ArMModel.construct(queryString);
-        return Response
-                .ok(result)
-                .build();
-    }
     private String queryString(String rid, String season) {
        return Config.prefix
-                + "CONSTRUCT { \r\n" 
-		+ " ?s ?p1 ?o1 .  \r\n"
-		+ " ?o1 ?p2 ?o2 . \r\n"
-                + "} WHERE { \r\n " 
-                + " ?s arm:isCharacter " + rid + " . \r\n"
-		+ " ?s arm:atSeasonTime arm:" + season + " . \r\n"
-		+ " ?s rdf:type arm:CharacterSheet . \r\n"
-		+ " ?s ?p1 ?o1 . \r\n"
-		+ " NOT EXISTS { ?p1 rdf:type arm:ignoredProperty }  \r\n"
-                + " OPTIONAL { \r\n" 
-                + "     ?o1 ?p2 ?o2 . \r\n" 
-		+ "     NOT EXISTS { ?o1 rdf:type arm:CharacterSheet }  \r\n"
-		+ "     NOT EXISTS { ?p2 rdf:type arm:ignoredProperty }  \r\n"
-                + " } \r\n"
-                + "}";
-       } 
-    @GET
-    @Path("/unframed/{id}/{season}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getCharacter(@PathParam("id") String id,
-                                 @PathParam("season") String season ) {
-
-        String rid = "armchar:" + id ;
-        String result = ArMModel.construct(this.queryString(rid,season));
-        return Response
-                .ok(result)
-                .build();
-    }
-    @GET
-    @Path("/{id}/{season}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response testCharacter(@PathParam("id") String id,
-                                 @PathParam("season") String season ) 
-				 throws IOException {
-
-        String frame = Config.getInstance().charsheetframe ;
-        String rid = "armchar:" + id ;
-        String queryString = Config.prefix
                 + "CONSTRUCT { \r\n" 
 		+ " ?sheet a arm:CharacterSheet . \r\n"
 		+ " ?sheet ?dp ?dval .  \r\n"
@@ -139,7 +78,17 @@ public class Character {
                 + " ?oval a ?type . \r\n" 
 		+ " ?obp a owl:DatatypeProperty . \r\n"
                 + "}";
-        String result = ArMModel.construct(queryString,frame);
+       } 
+    @GET
+    @Path("/{id}/{season}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response testCharacter(@PathParam("id") String id,
+                                 @PathParam("season") String season ) 
+				 throws IOException {
+
+        String frame = Config.getInstance().charsheetframe ;
+        String rid = "armchar:" + id ;
+        String result = ArMModel.construct(this.queryString(rid,season),frame);
         return Response
                 .ok(result)
                 .build();
@@ -154,29 +103,7 @@ public class Character {
 
         String frame = Config.getInstance().charsheetframe ;
         String rid = "armchar:" + id ;
-        String queryString = Config.prefix
-                + "CONSTRUCT { \r\n" 
-		+ " ?sheet a arm:CharacterSheet . \r\n"
-		+ " ?sheet ?dp ?dval .  \r\n"
-		+ " ?sheet ?op ?oval . \r\n"
-                + " ?oval ?obp ?obval . \r\n" 
-                + " ?oval a ?type . \r\n" 
-		+ " ?sheet ?op ?oval . \r\n"
-                + "} WHERE { \r\n " 
-                + " ?sheet arm:isCharacter " + rid + " . \r\n"
-		+ " ?sheet arm:atSeasonTime arm:" + season + " . \r\n"
-		+ " ?sheet a arm:CharacterSheet . \r\n"
-		+ " ?sheet ?dp ?dval . \r\n"
-		+ " ?sheet ?op ?oval . \r\n"
-		+ " NOT EXISTS { ?oval a arm:CharacterSheet }\r\n"
-		+ " NOT EXISTS { ?op a arm:ignoredProperty }\r\n"
-		+ " ?dp a owl:DatatypeProperty . \r\n"
-		+ " ?op a owl:ObjectProperty . \r\n"
-                + " ?oval ?obp ?obval . \r\n" 
-                + " ?oval a ?type . \r\n" 
-		+ " ?obp a owl:DatatypeProperty . \r\n"
-                + "}";
-        String result = ArMModel.construct(queryString);
+        String result = ArMModel.construct(this.queryString(rid,season));
         return Response
                 .ok(result)
                 .build();
