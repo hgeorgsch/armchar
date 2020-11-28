5a680992a3f1a6829429bbdaf90bc6b0987c7c48
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-28 17:53:39 +0100

    Split rules

---
+++ Ontology/basic.r
@@ -0,0 +1,45 @@
+
+[ newflaw: ( ?x arm:hasNewFlaw ?o ) -> ( ?x arm:hasFlaw ?o ) ]
+[ newvirtue: ( ?x arm:hasNewVirtue ?o ) -> ( ?x arm:hasVirtue ?o ) ]
+[ vfp:
+  ( ?vc arm:grantsPTraitClass ?pc )
+  ( ?v  rdf:type ?vc )
+  ( ?v  rdf:type armr:minorFlaw )
+  makeTemp( ?p ) 
+  ->
+  ( ?p rdf:type ?pc )
+  ( ?p rdf:type arm:PersonalityTrait )
+  ( ?v arm:grantsPTrait ?p )
+  ( ?p arm:hasScore +3 )
+  ]
+[ vfpmajor:
+  ( ?vc  arm:grantsPTraitClass ?pc )
+  ( ?v  rdf:type ?vc )
+  ( ?v  rdf:type armr:majorFlaw )
+  makeTemp( ?p ) 
+  ->
+  ( ?p rdf:type ?pc )
+  ( ?p rdf:type arm:PersonalityTrait )
+  ( ?v arm:grantsPTrait ?p )
+  ( ?p arm:hasScore +6 )
+  ]
+[ vfv:
+  ( ?vc1  arm:grantsVirtueClass ?vc2 )
+  ( ?v1  rdf:type ?vc1 )
+  makeTemp( ?v2 ) 
+  ->
+  ( ?v2 rdf:type ?vc2 )
+  ( ?v2 rdf:type arm:Virtue )
+  ( ?v1 arm:grantsVirtue ?v2 )
+  ( ?v2 arm:hasScore 0 )
+  ]
+[ vff:
+  ( ?vc1  arm:grantsFlawClass ?vc2 )
+  ( ?v1  rdf:type ?vc1 )
+  makeTemp( ?v2 ) 
+  ->
+  ( ?v2 rdf:type ?vc2 )
+  ( ?v2 rdf:type arm:Flaw )
+  ( ?v1 arm:grantsFlaw ?v2 )
+  ( ?v2 arm:hasScore 0 )
+  ]
--- Ontology/vf.r
+++ Ontology/vf.r
@@ -1,50 +1,3 @@
-
-[ vfp:
-  ( ?vc  arm:grantsPTraitClass ?pc )
-  ( ?v  rdf:type ?vc )
-  ( ?v  rdf:type armr:minorFlaw )
-  makeTemp( ?p ) 
-  ->
-  ( ?p rdf:type ?pc )
-  ( ?p rdf:type arm:PersonalityTrait )
-  ( ?v arm:grantsPTrait ?p )
-  ( ?v arm:grantsTrait ?p )
-  ( ?p arm:hasScore +3 )
-  ]
-[ vfpmajor:
-  ( ?vc  arm:grantsPTraitClass ?pc )
-  ( ?v  rdf:type ?vc )
-  ( ?v  rdf:type armr:majorFlaw )
-  makeTemp( ?p ) 
-  ->
-  ( ?p rdf:type ?pc )
-  ( ?p rdf:type arm:PersonalityTrait )
-  ( ?v arm:grantsPTrait ?p )
-  ( ?v arm:grantsTrait ?p )
-  ( ?p arm:hasScore +6 )
-  ]
-[ vfv:
-  ( ?vc1  arm:grantsVirtueClass ?vc2 )
-  ( ?v1  rdf:type ?vc1 )
-  makeTemp( ?v2 ) 
-  ->
-  ( ?v2 rdf:type ?vc2 )
-  ( ?v2 rdf:type arm:Virtue )
-  ( ?v1 arm:grantsVirtue ?v2 )
-  ( ?v arm:grantsTrait ?p )
-  ( ?v2 arm:hasScore 0 )
-  ]
-[ vff:
-  ( ?vc1  arm:grantsFlawClass ?vc2 )
-  ( ?v1  rdf:type ?vc1 )
-  makeTemp( ?v2 ) 
-  ->
-  ( ?v2 rdf:type ?vc2 )
-  ( ?v2 rdf:type arm:Flaw )
-  ( ?v1 arm:grantsFlaw ?v2 )
-  ( ?v arm:grantsTrait ?p )
-  ( ?v2 arm:hasScore 0 )
-  ]
 [ vfability:
   ( ?vclass  arm:grantsAbilityClass ?abclass )
   ( ?v  rdf:type ?vclass )
@@ -52,8 +5,9 @@
   ->
   ( ?ab rdf:type ?abclass )
   ( ?ab rdf:type arm:Ability )
-  ( ?v arm:grantsTrait ?p )
+  ( ?p rdf:type arm:Trait )
   ( ?v arm:grantsAbility ?ab )
+  ( ?v arm:grantsTrait ?p )
   ( ?ab arm:hasTotalXP 5 )
   ]
 [ advancevfgrant:
@@ -64,22 +18,25 @@
   ( ?adv arm:advanceTrait ?ab )
   ]
 [ chargenvfgrant:
-  ( ?v  arm:grantsTrait ?ab )
   ( ?cs  arm:hasNewVirtue ?v )
+  ( ?cs  rdf:type arm:CharacterSheet )
+  ( ?v  arm:grantsTrait ?ab )
   ->
   ( ?cs arm:hasTrait ?ab )
   ]
 [ chargenvfgrantv:
-  ( ?v  arm:grantsVirtue ?ab )
   ( ?cs  arm:hasNewVirtue ?v )
+  ( ?cs  rdf:type arm:CharacterSheet )
+  ( ?v  arm:grantsVirtue ?ab )
   ->
   ( ?cs arm:hasVirtue ?ab )
   ( ?cs arm:hasNewVirtue ?ab )
   ]
 
 [ chargenvfgrantf:
-  ( ?v  arm:grantsFlaw ?ab )
   ( ?cs  arm:hasNewFlaw ?v )
+  ( ?cs  rdf:type arm:CharacterSheet )
+  ( ?v  arm:grantsFlaw ?ab )
   ->
   ( ?cs arm:hasFlaw ?ab )
   ( ?cs arm:hasNewFlaw ?ab )
