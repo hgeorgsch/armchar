5d93445351977352ef9f24a2ba5b1da0b61bfd0d
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-11-28 15:58:06 +0100

    Fixed logic errors.  Not working yet, I think

--- Ontology/vf.r
+++ Ontology/vf.r
@@ -1,11 +1,45 @@
 
-[ vfvf:
+[ vfp:
+  ( ?vc  arm:grantsPTraitClass ?pc )
+  ( ?v  rdf:type ?vc )
+  ( ?v  rdf:type armr:minorFlaw )
+  makeTemp( ?p ) 
+  ->
+  ( ?p rdf:type ?pc )
+  ( ?p rdf:type arm:Flaw )
+  ( ?v arm:grantsPTrait ?p )
+  ( ?p arm:hasScore +3 )
+  ]
+[ vfpmajor:
+  ( ?vc  arm:grantsPTraitClass ?pc )
+  ( ?v  rdf:type ?vc )
+  ( ?v  rdf:type armr:majorFlaw )
+  makeTemp( ?p ) 
+  ->
+  ( ?p rdf:type ?pc )
+  ( ?p rdf:type arm:Flaw )
+  ( ?v arm:grantsPTrait ?p )
+  ( ?p arm:hasScore +6 )
+  ]
+[ vfv:
   ( ?vc1  arm:grantsVirtueClass ?vc2 )
   ( ?v1  rdf:type ?vc1 )
   makeTemp( ?v2 ) 
   ->
   ( ?v2 rdf:type ?vc2 )
+  ( ?v2 rdf:type arm:Virtue )
   ( ?v1 arm:grantsVirtue ?v2 )
+  ( ?v2 arm:hasScore 0 )
+  ]
+[ vff:
+  ( ?vc1  arm:grantsFlawClass ?vc2 )
+  ( ?v1  rdf:type ?vc1 )
+  makeTemp( ?v2 ) 
+  ->
+  ( ?v2 rdf:type ?vc2 )
+  ( ?v2 rdf:type arm:Flaw )
+  ( ?v1 arm:grantsFlaw ?v2 )
+  ( ?v2 arm:hasScore 0 )
   ]
 [ vfability:
   ( ?vclass  arm:grantsAbilityClass ?abclass )
@@ -13,6 +47,7 @@
   makeTemp( ?ab ) 
   ->
   ( ?ab rdf:type ?abclass )
+  ( ?ab rdf:type arm:Ability )
   ( ?v arm:grantsAbility ?ab )
   ( ?ab arm:hasTotalXP 5 )
   ]
@@ -33,6 +68,15 @@
   ( ?v  arm:grantsVirtue ?ab )
   ( ?cs  arm:hasNewVirtue ?v )
   ->
-  ( ?cs arm:hasNewVirtue ?ab )
   ( ?cs arm:hasVirtue ?ab )
+  ( ?cs arm:hasNewVirtue ?ab )
   ]
+
+[ chargenvfgrantf:
+  ( ?v  arm:grantsFlaw ?ab )
+  ( ?cs  arm:hasNewFlaw ?v )
+  ->
+  ( ?cs arm:hasFlaw ?ab )
+  ( ?cs arm:hasNewFlaw ?ab )
+  ]
+
