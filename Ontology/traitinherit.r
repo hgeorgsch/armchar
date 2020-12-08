792636da50e6f8eee2b6c8a5e070f120fef47796
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-12-08 21:19:24 +0100

    Simplifications

--- Ontology/traitinherit.r
+++ Ontology/traitinherit.r
@@ -8,16 +8,3 @@
   noValue( ?s,?p ) 
   -> ( ?s ?p ?o ) 
       ]
-[ traitdescription:
-  ( ?t rdf:type ?c ) ( ?c rdf:type arm:LeafTraitClass ) 
-  ( ?c arm:hasDescription ?d ) 
-  -> ( ?t arm:hasGeneralDescription ?d ) ]
-# TODO: spelldescription should be a special case of leaftraitdescription
-[ spelldescription:
-  ( ?t rdf:type ?c ) ( ?c rdf:type arm:SpellClass ) 
-  ( ?c arm:hasDescription ?d ) 
-  -> ( ?t arm:hasGeneralDescription ?d ) ]
-[ leaftraitdescription:
-  ( ?c rdf:type arm:LeafTraitClass ) noValue( ?c, arm:hasDescription ) 
-  ( ?c rdfs:subClassOf ?s ) ( ?s arm:hasDescription ?d ) 
-  -> ( ?c arm:hasDescription ?d ) ]
