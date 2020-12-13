2117ff2c0b2e1d141a1a3c8fa0a4b7d64984ddd0
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2020-12-13 14:37:30 +0100

    Trying to get a working advancement service

--- load.sh
+++ load.sh
@@ -1,3 +1,4 @@
+curl http://localhost:8080/armchar/Advancement/cieran > adv.json
 curl http://localhost:8080/armchar/Character/cieran > cieran.json
 curl http://localhost:8080/armchar/Character/cieran/1217/Summer > summer1217.json
 
