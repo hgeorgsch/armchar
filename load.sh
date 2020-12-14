55d4aafdb216eb19da3d5da1922b28593db4e27c
Hans Georg Schaathun <georg@schaathun.net>
2020-12-14 14:59:52 +0000

    typo

--- load.sh
+++ load.sh
@@ -1,4 +1,6 @@
-curl http://localhost:8080/armchar/Advancement/cieran > adv.json
+# curl http://localhost:8080/armchar/Advancement/cieran > adv.json
+curl http://localhost:8080/armchar/Advancement/framed/cieran > framed.json
+curl http://localhost:8080/armchar/Advancement/unframed/cieran > unframed.json
 curl http://localhost:8080/armchar/Character/cieran > cieran.json
 curl http://localhost:8080/armchar/Character/cieran/1217/Summer > summer1217.json
 curl http://localhost:8080/armchar/dump/jsonld-inf > dump.json
