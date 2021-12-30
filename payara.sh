acca5f6db8c89d7e2c2747854f65975eb0c68407
Hans Georg Schaathun <georg@nordstrom.schaathun.net>
2021-12-30 23:15:28 +0100

    Fixed makefile and scripts to run payara.

--- payara.sh
+++ payara.sh
@@ -6,6 +6,7 @@
 mvn install || exit
 docker stop armchar
 docker rm armchar
+mkdir -p payara
 cp target/armchar-1.0-SNAPSHOT.war payara/armchar.war
 docker run --name armchar -p 8080:8080 -v `pwd`/payara:/opt/payara/deployments -v `pwd`/serverdata:/opt/payara/serverdata  -v `pwd`/tdb:/opt/payara/tdb  armchar
 
